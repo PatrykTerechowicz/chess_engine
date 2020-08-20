4473c558e72151ddb99d29cd57bdc0adf288e9b5
Patryk Terechowicz <patryk.terechowicz1@gmail.com>
2020-08-20 11:54:24 +0200

    Created new UI with menu, options and possibility of starting multiple games.

---
+++ app.py
@@ -0,0 +1,51 @@
+from sys import exit
+from tkinter import *
+from pathlib import Path
+from gui_board import Board
+import res.resource_manager
+
+
+def exit_popup():
+    sys.exit(0)
+
+
+class App:
+    def __init__(self):
+        self.top_frame = Tk()
+        self.top_frame.title("Chess Game")
+        self.top_frame.geometry("200x100")
+        self.create_main_menu()
+        self.is_game_started = False
+
+    def create_new_game(self):
+        if self.is_game_started:
+            self.game_already_started_popup()
+            return
+        new_window = Toplevel(self.top_frame)
+        new_window.title("Gra człowiek kontra komputer")
+        new_window.geometry("450x450")
+        Board(new_window).pack(expand=YES, fill=BOTH)
+        new_window.mainloop()
+
+    def game_already_started_popup(self):
+        pass
+
+    def open_options_menu(self):
+        pass
+
+    def create_main_menu(self):
+        img = PhotoImage()
+        label = Label(self.top_frame)
+        label['text'] = "Szachy by patryk"
+        label.pack()
+        Button(text="New Game", command=self.create_new_game).pack(fill=BOTH)
+        Button(text="Options", command=self.open_options_menu).pack(fill=BOTH)
+        Button(text="Exit", command=exit_popup).pack(fill=BOTH)
+
+    def start(self):
+        self.top_frame.mainloop()
+
+
+if __name__ == '__main__':
+    application = App()
+    application.start()
---
+++ gui/gui_board.py
@@ -0,0 +1,54 @@
+from tkinter import *
+from res import resource_manager
+from pieces import *
+from player import Player
+
+
+CHOOSE_PIECE = 0
+SELECT_CELL = 1
+
+
+class BoardButton(Button):
+    def __init__(self, parent, x, y):
+        super(BoardButton, self).__init__(parent)
+        self.board = parent
+        self.x = x
+        self.y = y
+        self['command'] = self.callback
+        self.image = PhotoImage(resource_manager.empty)
+        self['image'] = self.image
+        self['width'] = 50
+        self['height'] = 50
+
+    def callback(self):
+        state = self.board.state
+        if state == CHOOSE_PIECE:
+            pass
+        if state == SELECT_CELL:
+            pass
+        print("Clicked a button " + str(self) + f" {self.x} {self.y}")
+
+    def place_piece(self, piece: Piece):
+        pass
+
+
+class Board(Frame):
+    def __init__(self, parent):  # defines empty board
+        super(Board, self).__init__(parent)
+        self.state = CHOOSE_PIECE
+        self.player_playing = Player.WHITE
+        self.buttons = []
+        for y in range(0, 8):
+            for x in range(0, 8):
+                new_button = BoardButton(self, x, y)
+                self.buttons.append(new_button)
+                new_button.grid(column=x, row=y)
+
+    def place_starting_pieces(self):
+        pass
+
+    def get_button(self, x: int, y: int):
+        return self.buttons[x + y * 8]
+
+    def save_board(self):
+        pass
